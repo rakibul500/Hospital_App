c429b2680109dbfed860e7693d771aa456633240
rakibul <ahmedsawon204@gmail.com>
2021-07-10 00:18:34 +0600

    Complete Loading Page

---
+++ app/src/main/java/com/example/hospital/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.hospital;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN=5000;
+
+    //variable
+    Animation topAnim,bottomAnim;
+    ImageView logo;
+    TextView logo_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //animation
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.botttom_animation);
+
+        logo=findViewById(R.id.logo_image);
+        logo_text=findViewById(R.id.logo_text);
+
+        logo.setAnimation(topAnim);
+        logo_text.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,Home.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+
+    }
+}
