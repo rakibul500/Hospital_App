85ef97ae1a706bc8c6233ce39967756d67b4f965
rakibul <ahmedsawon204@gmail.com>
2021-07-12 01:13:43 +0600

    Hospital1 Onclick implement Done

--- app/src/main/java/com/example/hospital/Home.java
+++ app/src/main/java/com/example/hospital/Home.java
@@ -2,7 +2,11 @@ package com.example.hospital;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.denzcoskun.imageslider.ImageSlider;
@@ -16,9 +20,10 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements View.OnClickListener{
 
     ImageSlider main_slider;
+    CardView button1,button2,button3,button4,button5,button6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +31,32 @@ public class Home extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home);
 
+        //button hooks
+        button1=findViewById(R.id.buttonId1);
+        button2=findViewById(R.id.buttonId2);
+        button3=findViewById(R.id.buttonId3);
+        button4=findViewById(R.id.buttonId4);
+        button5=findViewById(R.id.buttonId5);
+        button6=findViewById(R.id.buttonId6);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+
+        //slider Hooks
         main_slider=findViewById(R.id.image_slider);
+
         final List<SlideModel> remote_images=new ArrayList<>();
 
+        //CallFirebase
         FirebaseDatabase.getInstance().getReference().child("Slider")
-                .addListenerForSingleValueEvent(new ValueEventListener() {
+                .addListenerForSingleValueEvent(new ValueEventListener()
+
+
+                {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         for(DataSnapshot data:snapshot.getChildren())
@@ -48,5 +74,19 @@ public class Home extends AppCompatActivity {
 
                     }
                 });
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()){
+
+            case R.id.buttonId1:
+                i=new Intent(this,Hospital1.class);
+                startActivity(i);
+                break;
+
+
+        }
     }
 }
--- app/src/main/java/com/example/hospital/MainActivity.java
+++ app/src/main/java/com/example/hospital/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN=5000;
+    private static int SPLASH_SCREEN=3000;
 
     //variable
     Animation topAnim,bottomAnim;
